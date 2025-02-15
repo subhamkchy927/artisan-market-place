10a839a4631e40ba733302cd333198a01a870c33
ShubhamK <subhamkchy927@gmail.com>
2025-02-15 17:30:37 +0530

    Seller Details Service (API)

--- src/main/java/com/artisan_market_place/service/SellerDetailsService.java
+++ src/main/java/com/artisan_market_place/service/SellerDetailsService.java
@@ -4,6 +4,7 @@ import com.artisan_market_place.requestDto.SellerRequestDto;
 import com.artisan_market_place.responseDto.SellerResponseDto;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -11,6 +12,6 @@ public interface SellerDetailsService {
     public SellerResponseDto createSeller(SellerRequestDto dto);
     public SellerResponseDto updateSeller(SellerRequestDto dto, Long sellerId);
     public SellerResponseDto getSellerById(Long sellerId);
-    public SellerResponseDto deleteSeller(Long sellerId);
+    public HashMap<String,String> deleteSeller(Long sellerId);
     public List<SellerResponseDto> getAllSeller(Boolean isApplicationAdmin);
 }
