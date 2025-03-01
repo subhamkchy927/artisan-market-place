f0770833a18b2900a8520b3e56ea634bf32ac0fa
ShubhamK <subhamkchy927@gmail.com>
2025-03-01 18:38:12 +0530

    Sendgrid email

---
+++ src/main/java/com/artisan_market_place/service/SendGridApiService.java
@@ -0,0 +1,14 @@
+package com.artisan_market_place.service;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public interface SendGridApiService {
+
+    public void sendEmail(String toEmail, String subject, String message) throws IOException;
+
+}
