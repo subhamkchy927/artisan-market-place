5cd6d88d557999ddbf4a888392e77c16e4b28289
ShubhamK <subhamkchy927@gmail.com>
2025-03-01 18:38:12 +0530

    Sendgrid email

---
+++ src/main/java/com/artisan_market_place/Configurations/SendGridConfig.java
@@ -0,0 +1,18 @@
+package com.artisan_market_place.Configurations;
+
+import com.sendgrid.SendGrid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SendGridConfig {
+    @Value("${sendgrid.api-key}")
+    private String sendGridApiKey;
+
+    @Bean
+    public SendGrid sendGridClient() {
+        SendGrid sendGrid = new SendGrid(sendGridApiKey);
+        return sendGrid;
+    }
+}
---
+++ src/main/java/com/artisan_market_place/serviceImpl/SendGridApiServiceImpl.java
@@ -0,0 +1,40 @@
+package com.artisan_market_place.serviceImpl;
+
+import com.artisan_market_place.service.SendGridApiService;
+import com.sendgrid.SendGrid;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+
+@Service
+public class SendGridApiServiceImpl implements SendGridApiService {
+
+    @Value("${sendgrid.from}")
+    private String fromEmail;
+    private final SendGrid sendGrid;
+
+    public SendGridApiServiceImpl(SendGrid sendGrid) {
+        this.sendGrid = sendGrid;
+    }
+
+    @Override
+    public void sendEmail(String toEmail, String subject, String message) throws IOException {
+        Email from = new Email(fromEmail);
+        Email to = new Email(toEmail);
+        Content content = new Content("text/plain", message);
+        Mail mail = new Mail(from, subject, to, content);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            sendGrid.api(request);
+        } catch (IOException ex) {
+            throw new RuntimeException("Error sending email", ex);
+        }
+    }
+}
